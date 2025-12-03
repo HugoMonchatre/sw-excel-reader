dda1ef2b0ea347a70caf184015f7b1cbb655fac0
Hugo Monchatre <hugmonch@f1r10s1.clusters.42paris.fr>
2025-12-03 12:16:28 +0100

    Revert to CommonJS - remove type module

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import https from 'https';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const express = require('express');
+const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
